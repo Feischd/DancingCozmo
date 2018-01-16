186c83c1bd335faf43a51af2a09491f5b5deef9f
feischd <feist-alex@t-online.de>
2018-01-16 10:13:11 +0100

    Änderung der Speicherdurchsuchung auf Datei-Endungen

--- Test/src/TestMain.java
+++ Test/src/TestMain.java
@@ -5,10 +5,11 @@ public class TestMain {
 	public static void main(String[] args) {
 		
 		//Nach einem File suchen in einem Verzeichnis
-		File a = new File("C:\\Users\\Alexander Feist\\Desktop");
+		File dir = new File("C:\\Users\\Alexander Feist\\Desktop");
 		//System.out.println("Test");
-		String b = "Akku!.txt";
-		TestMethoden.searchFile(a, b);
+//		String find = "Akku!.txt";
+		String find = ".txt";
+		TestMethoden.searchFile(dir, find);
 	}
 
 }
--- Test/src/TestMethoden.java
+++ Test/src/TestMethoden.java
@@ -3,25 +3,39 @@ import java.util.ArrayList;
 
 public class TestMethoden {
 
-	//Methode um nach einem bestimmten File in einem bestimmten Verzeichnis suchen
+	// Methode um nach einem bestimmten File in einem bestimmten Verzeichnis suchen
 	public static ArrayList<File> searchFile(File dir, String find) {
 
+		int zaehler = 0;
+		
 		File[] files = dir.listFiles();
 		ArrayList<File> matches = new ArrayList<File>();
 		if (files != null) {
 			for (int i = 0; i < files.length; i++) {
-				if (files[i].getName().equalsIgnoreCase(find)) { // überprüft ob der Dateiname mit dem Suchstring
-					// übereinstimmt. Groß-/Kleinschreibung wird
-					// ignoriert.
+//				if (files[i].getName().equalsIgnoreCase(find)) { // überprüft ob der Dateiname mit dem Suchstring
+//					// übereinstimmt. Groß-/Kleinschreibung wird
+//					// ignoriert.
+//					matches.add(files[i]);
+//					System.out.println("Treffer");
+//				} 
+				if(files[i].getName().endsWith(find)) {
 					matches.add(files[i]);
-					System.out.println("Treffer");
+					zaehler = zaehler + 1;
+//					System.out.println("Treffer");
 				}
+				
+//				else {
+//					System.out.println("kein Treffer");
+//				}
 				if (files[i].isDirectory()) {
 					matches.addAll(searchFile(files[i], find)); // fügt der ArrayList die ArrayList mit den
 					// Treffern aus dem Unterordner hinzu
 				}
 			}
 		}
+		//Achtung! Ausgabe auch für jedes Verzeichnis, die letzte Ausgabe ist das 
+		//Hauptverzeichnis
+		System.out.println("Anzahl gefundene Dateien: " + zaehler);
 		return matches;
 	}
 
