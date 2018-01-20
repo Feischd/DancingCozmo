1a21c8e98358fe31ad3fd0505e5cb9b91f79f9dc
Paddy98 <pfetzerp@dhbw-loerrach.de>
2018-01-20 17:05:15 +0100

    test external programs

--- Java-Cozmo/src/Java_Cozmo.java
+++ Java-Cozmo/src/Java_Cozmo.java
@@ -3,4 +3,7 @@ public class Java_Cozmo {
     	System.out.println("Test");
     	//Was isch da los? Warum gehts net?
     }
+
+    // Starting external programs from java
+    // http://openbook.rheinwerk-verlag.de/javainsel9/javainsel_11_005.htm
 }
