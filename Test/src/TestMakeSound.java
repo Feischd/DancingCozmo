4d9434f5c79f003c1c51c043911dc5092e24b5ca
Paddy98 <pfetzerp@dhbw-loerrach.de>
2018-01-23 12:35:06 +0100

    makeSound

--- Test/src/TestMakeSound.java
+++ Test/src/TestMakeSound.java
@@ -1,5 +1,8 @@
 public class TestMakeSound {
 
+    // The MakeSound class only works by using the wav format, so there should be a converter to get the selected file into wav-format.
+    // For implementation this possibly helps:
+    // https://docs.oracle.com/javase/tutorial/sound/converters.html
     public static void main(String[]args) {
         MakeSound ms = new MakeSound();
         ms.playSound("01_Titelnummer_1.wav");
