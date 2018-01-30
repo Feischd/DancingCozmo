d0f0e22fc6365e983451c8a86788094cc5703904
moonfiretiger <ramonabecker1999@gmail.com>
2018-01-30 11:22:51 +0100

    Python DarkMusik, Disco, Electro, Folk, Funk DarkMusik ist noch nicht fertig ob alles funktioniert wurde noch nicht geprüft

--- Python-Cozmo/src/Cozmo_Python/test.py
+++ Python-Cozmo/src/Cozmo_Python/test.py
@@ -1,35 +1,36 @@
-#!/usr/bin/env python3
+# falls der Lift oben ist wird er runter geholt
+robot.set_lift_height(0.0).wait_for_completed()
 
-# Copyright (c) 2016 Anki, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License in the file LICENSE.txt or at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# faehrt 150 millimeters vorwaerts mit 50 millimeters-per-second.
+robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
 
-'''Drive And Turn
+# faehrt 150 millimeters rueckwaerts mit 50 millimeters-per-second.
+robot.drive_straight(distance_mm(-150), speed_mmps(50)).wait_for_completed()
 
-Make Cozmo drive forwards and then turn 90 degrees to the left.
-'''
+# Lift geht hoch und wieder runter (at 5 radians per second)
+robot.move_lift(5)
+time.sleep(3)
+robot.move_lift(-5)
+time.sleep(3)
 
-import cozmo
-from cozmo.util import degrees, distance_mm, speed_mmps
+# dreht sich um 45° nach links
+robot.turn_in_place(degrees(45)).wait_for_completed()
+# dreht sich um 90° nach links
+robot.turn_in_place(degrees(90)).wait_for_completed()
+# dreht sich um 180° nach links
+robot.turn_in_place(degrees(180)).wait_for_completed()
+# dreht sich um 270° nach links
+robot.turn_in_place(degrees(270)).wait_for_completed()
+# dreht sich um 360° nach links
+robot.turn_in_place(degrees(360)).wait_for_completed()
 
-
-def cozmo_program(robot: cozmo.robot.Robot):
-    # Drive forwards for 150 millimeters at 50 millimeters-per-second.
-    robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
-
-    # Turn 90 degrees to the left.
-    # Note: To turn to the right, just use a negative number.
-    robot.turn_in_place(degrees(90)).wait_for_completed()
-
-
-cozmo.run_program(cozmo_program)
+# dreht sich um 45° nach rechts
+robot.turn_in_place(degrees(-45)).wait_for_completed()
+# dreht sich um 90° nach rechts
+robot.turn_in_place(degrees(-90)).wait_for_completed()
+# dreht sich um 180° nach rechts
+robot.turn_in_place(degrees(-180)).wait_for_completed()
+# dreht sich um 270° nach rechts
+robot.turn_in_place(degrees(-270)).wait_for_completed()
+# dreht sich um 360° nach rechts
+robot.turn_in_place(degrees(-360)).wait_for_completed()
