8d6948abbcbccba3df0b8dc9e8aaa107ebc6808d
feischd <feist-alex@t-online.de>
2018-02-13 11:46:54 +0100

    .

--- Main/src/Controller.java
+++ Main/src/Controller.java
@@ -1,5 +1,5 @@
 import javafx.scene.layout.AnchorPane;
-
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -7,17 +7,45 @@ import javafx.scene.image.ImageView;
 
 public class Controller{
 
-	private AnchorPane AnchorP;
-	private TextArea TextLiednamen;
-	private ImageView Bild;
-	private TextField Titel;
-	private TextField Kuenstler;
-	private TextField Genre;
-	private TextField Jahr;
-	private TextField Album;
-	private TextArea Sonstiges;
-	private Button Stop;
-	private Button Play;
+	@FXML
+	AnchorPane AnchorP;
+	
+	@FXML
+	TextArea TextLiednamen;
+	
+	@FXML
+	ImageView Bild;
+	
+	@FXML
+	TextField Titel;
+	
+	@FXML
+	TextField Kuenstler;
+	
+	@FXML
+	TextField Genre;
+	
+	@FXML
+	TextField Jahr;
+	
+	@FXML
+	TextField Album;
+	
+	@FXML
+	TextArea Sonstiges;
+	
+	@FXML
+	Button Stop;
+	
+	@FXML
+	Button Play;
+	
+//	@FXML
+//	protected void playClicked() {
+//		String text = TextLiednamen.getText();
+//		text = text + " !";
+//		TextLiednamen.setText(text);
+//	}
 	
 //	String titel;
 //	String kuenstler;
