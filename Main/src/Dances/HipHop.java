9f39f6d95518c05fb5a66ed8827227d98b079d11
moonfiretiger <ramonabecker1999@gmail.com>
2018-02-13 14:43:52 +0100

    Pyhton als Java DarkMusik, Disco, Electro, Folk, Funk, HipHop, Jazz, Klassik, Lateinamerikanisch

---
+++ Main/src/Dances/HipHop.java
@@ -0,0 +1,54 @@
+package Dances;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HipHop extends Dance {
+    public HipHop(){
+        laengeTanz = 10;
+    }
+
+    public void createDance(double laenge){
+        PrintWriter pWriter = null;
+        try {
+            // creating file
+            pWriter = new PrintWriter(new BufferedWriter(new FileWriter("cozmoDance.py")));
+
+            // python code
+            pWriter.println("import time");
+            pWriter.println("import cozmo");
+            pWriter.println("from cozmo.util import degrees, distance_mm, speed_mmps");
+            pWriter.println();
+
+            pWriter.println("def cozmo_program(robot: cozmo.robot.Robot):");
+            // falls der Lift oben ist wird er runter geholt
+            pWriter.println("   robot.set_lift_height(0.0).wait_for_completed()");
+            //dreht sich um 360° nach rechts
+            pWriter.println("   robot.turn_in_place(degrees(-360)).wait_for_completed()");
+            //dreht sich um 360° nach links
+            pWriter.println("   robot.turn_in_place(degrees(360)).wait_for_completed()");
+            //faehrt 100 millimeters vorwaerts mit 150 millimeters-per-second
+            pWriter.println("   robot.drive_straight(distance_mm(100), speed_mmps(150)).wait_for_completed()");
+            //faehrt 100 millimeters rueckwaerts mit 150 millimeters-per-second
+            pWriter.println("   robot.drive_straight(distance_mm(-100), speed_mmps(150)).wait_for_completed()");
+
+            pWriter.println();
+
+            pWriter.println("i = 0");
+            pWriter.println("while i < " + laenge + ":");
+            pWriter.println("   cozmo.run_program(cozmo_program)");
+            pWriter.println("   i = i + 1");
+
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            if (pWriter != null){
+                pWriter.flush();
+                pWriter.close();
+            }
+        }
+    }
+}
