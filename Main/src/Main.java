2b402f803e9dc709f34a97f2296f49a1c0e3a472
feischd <feist-alex@t-online.de>
2018-02-19 09:04:17 +0100

    .

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -17,6 +17,7 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -27,6 +28,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
