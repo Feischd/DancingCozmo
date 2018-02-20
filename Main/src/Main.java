dfb3b1e65d30317c0e44fc9bfa11e63e44015270
Paddy98 <pfetzerp@dhbw-loerrach.de>
2018-02-20 10:38:31 +0100

    main

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -269,72 +269,41 @@ public class Main extends Application {
 	// Ab hier Metadaten
 
 	private static void getMetadata(String fileLocation, int index) {
-		
-		/*try{
-			TimeUnit.SECONDS.sleep(2);
+		boolean converted = false;
+		boolean metadataAvailable = false;
+		Metadata metadata = null;
+		while(!converted || !metadataAvailable){
+			try{
+				InputStream input = new FileInputStream(new File(fileLocation));
+				converted = true;
 
-		}catch(Exception e){
-			System.out.println(e);
-		}*/
+				ContentHandler handler = new DefaultHandler();
+				metadata = new Metadata();
+				Parser parser = new Mp3Parser();
 
-		try {
-			TimeUnit.SECONDS.sleep(10);
-			InputStream input = new FileInputStream(new File(fileLocation));
-			ContentHandler handler = new DefaultHandler();
-			Metadata metadata = new Metadata();
-			Parser parser = new Mp3Parser();
+				ParseContext parseCtx = new ParseContext();
+				parser.parse(input, handler, metadata, parseCtx);
+				input.close();
 
-			ParseContext parseCtx = new ParseContext();
-			parser.parse(input, handler, metadata, parseCtx);
-			input.close();
+				if(metadata.size()>2){
+					metadataAvailable = true;
+				}
+			}catch(Exception e){
 
-			//System.out.println(metadata);
-			// List all metadata
-			//String[] metadataNames = metadata.names();
-
-//			for (String name : metadataNames) {
-//				System.out.println(name + ": " + metadata.get(name));
-//			}
-
-			// Retrieve the necessary info from metadata
-			// Names - title, xmpDM:artist etc. - mentioned below may differ based
-			// System.out.println("----------------------------------------------");
-			//System.out.println("Title: " + metadata.get("title"));
-			//System.out.println("Artists: " + metadata.get("xmpDM:artist"));
-			//System.out.println("Composer : " + metadata.get("xmpDM:composer"));
-			//System.out.println("Genre : " + metadata.get("xmpDM:genre"));
-			//System.out.println("Album : " + metadata.get("xmpDM:album"));
-
-			// addToTextArea(metadata.get("xmpDM:artist") + " " + metadata.get("title"));
-            System.out.println(metadata.get("title"));
-            System.out.println(metadata.get("xmpDM:artist") );
-			//spaeter noch machen
-			if((metadata.get("title") != null) && (metadata.get("xmpDM:artist") != null)){
-				
-			//songs.add(index, new Song(metadata.get("title"), metadata.get("xmpDM:artist"), fileLocation));
-			
-			//songs.set(index, ws.fillSongArray(songs.get(index)));
-
-
-
-            songs.add(ws.fillSongArray(new Song(metadata.get("title"), metadata.get("xmpDM:artist"), fileLocation)));
-
-			}else {
-//				songs.set(index, new Song("", "", ""));
-				songs.add(new Song("", "", fileLocation));
 			}
+		}
+		System.out.println(metadata.get("title"));
+		System.out.println(metadata.get("xmpDM:artist") );
 
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (TikaException e) {
-			e.printStackTrace();
-		} catch (Exception e){
-			e.printStackTrace();
+		if((metadata.get("title") != null) && (metadata.get("xmpDM:artist") != null)){
+			//songs.add(index, new Song(metadata.get("title"), metadata.get("xmpDM:artist"), fileLocation));
+			//songs.set(index, ws.fillSongArray(songs.get(index)));
+			songs.add(ws.fillSongArray(new Song(metadata.get("title"), metadata.get("xmpDM:artist"), fileLocation)));
+
+		}else {
+//				songs.set(index, new Song("", "", ""));
+			songs.add(new Song("", "", fileLocation));
 		}
 	}
 
